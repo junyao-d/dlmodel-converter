92dafa3f4b6c5f9e3f8e4dd791e4d9bca1a0a198
Junyao <jydeng233@gmail.com>
2021-06-12 19:00:51 +1000

    change keras converter to from_keras_model(model)

--- tflite_converter.py
+++ tflite_converter.py
@@ -16,26 +16,13 @@ def keras_to_tflite(file_path):
     model = tf.keras.models.load_model(file_path)
     filename = os.path.basename(file_path)
     filename_base = os.path.splitext(filename)[0]
-    saved_model_path = os.path.join(SAVEDMODEL_FOLDER, filename_base+'_saved_model')
-    tf.keras.models.save_model(model, saved_model_path)
-    has_saved_model = tf.saved_model.contains_saved_model(saved_model_path)
-    if has_saved_model:
-        print("saved modol exists")
-        print(saved_model_path)
-        converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_path)
-        ##############################################################################
-        # Dynamic range quantization 
-        converter.optimizations = [tf.lite.Optimize.DEFAULT]
-        # Float16 quantization 
-        converter.target_spec.supported_types = [tf.float16]
-        ##############################################################################
-        tflite_model = converter.convert()
-        output_file = os.path.join(OUTPUT_FOLDER, filename_base + '.tflite')
-        with open(output_file, 'wb') as f:
-            f.write(tflite_model)
-        print(output_file)
-    else:
-        print("saved model does not exist")
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+    output_file = os.path.join(OUTPUT_FOLDER, filename_base + '.tflite')
+    with open(output_file, 'wb') as f:
+        f.write(tflite_model)
+    print(output_file)
+
 
 
 def tensorflow_to_tflite(file_path):
